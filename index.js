3ca10812c3bffd2bcefa537f1ec06d620e8cf22f
abdool101 <abdoolrazaktasiu101@gmail.com>
2026-07-15 08:39:35 +0100

    Create index.js

---
+++ index.js
@@ -0,0 +1,7 @@
+console.log("==================================");
+console.log(" WhatsApp Bot is Starting...");
+console.log("==================================");
+
+setInterval(() => {
+    console.log("✅ Bot is running...");
+}, 30000);
